86dedf37c6995563e9f75884dca249398999f1cf
Timur <kirichenkotimur@gmail.com>
2025-04-04 00:27:02 +0300

    Test Back and DB

--- Backend/back/back/settings.py
+++ Backend/back/back/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,8 +86,12 @@ WSGI_APPLICATION = 'back.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ.get("MYSQL_DATABASE"),
+            'USER': os.environ.get("MYSQL_USER"),
+            'PASSWORD': os.environ.get("MYSQL_PASSWORD"),
+            'HOST': 'db',  # docker-compose service name 'db' resolves to host name 'db'
+            'PORT': 3306
     }
 }
 
